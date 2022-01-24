dfaa6b199811379380fdbbcbef334a78eac2f1ac
Kjimoh <kjimoh@terragonltd.com>
2022-01-24 23:48:34 +0100

    added node class

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
 // const assert = require("assert");
 
-const Node = (operator, value, left, right) => {
-  const result = function () {
-    switch (this.operator) {
-      case "+":
-        return left.result() + right.result();
-      case "-":
-        return left.result() - right.result();
-      case "x":
-        return left.result() * right.result();
-      case "÷":
-        return left.result() / right.result();
-      default:
-        return value;
-    }
-  };
-
-  const toString = function () {
-    if (this.operator) {
-        return `(${this.left.toString()} ${this.operator} ${this.right.toString()})`;
+class Node {
+    constructor(operator, value, left, right) {
+        this.operator = operator;
+        this.value = value;
+        this.left = left;
+        this.right = right;
       }
-      return this.value.toString();
-  };
 
-  return {
-    operator,
-    value,
-    left,
-    right,
-    result,
-    toString
-  };
-};
+      toString() {
+        if (this.operator) {
+          return `(${this.left.toString()} ${this.operator} ${this.right.toString()})`;
+        }
+        return this.value.toString();
+      }
+}
+
+
+
+// const Node = (operator, value, left, right) => {
+//   const result = function () {
+//     switch (this.operator) {
+//       case "+":
+//         return left.result() + right.result();
+//       case "-":
+//         return left.result() - right.result();
+//       case "x":
+//         return left.result() * right.result();
+//       case "÷":
+//         return left.result() / right.result();
+//       default:
+//         return value;
+//     }
+//   };
+
+//   const toString = function () {
+//     if (this.operator) {
+//         return `(${this.left.toString()} ${this.operator} ${this.right.toString()})`;
+//       }
+//       return this.value.toString();
+//   };
+
+//   return {
+//     operator,
+//     value,
+//     left,
+//     right,
+//     result,
+//     toString
+//   };
+// };
 
 const tree = Node(
   "÷",
